64de5e7b5d2a74fc70d0dcb77586d61d4451087a
rajasekhar <rajasekhar@channelkonnect.com>
2024-02-29 21:37:41 +0530

    setup

---
+++ setup/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update package lists and upgrade installed packages
+apt-get update -y && apt-get upgrade -y
+
+# Install unzip package
+apt install unzip -y
+
+# Install AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Check kubectl version
+kubectl version --short --client
+kubectl version --client
+
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+# Check eksctl version
+eksctl version
